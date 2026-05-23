7f44a068ac88908d327e5c39a762bfb6c13a08ac
sabavithu05 <CCCUser@cn.lan>
2026-05-23 16:09:48 +0530

    “hello-react”

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import themeimg from './themeimg.jpg';
 
 function App() {
+  const message = 'Hello, React!!!';
+  const paragraph = 'This is a simple React application.';
+  const name = 'vithusha';  
+
+
+  const PI = 3.14159;
+  const description = `The value of PI is approximately ${PI}.`;
+
+  const theme = <img src={themeimg} alt="website" />;
+  
+  const  welcome =(props)= {  
+    return <div> {props.childern}! </div>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <welcome> Welcome to React!
+        <ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>  
+         </welcome>
+        <h1> {message} </h1>
+        <hi>Hello {name}</hi>
+        <p> {paragraph} </p>
+        <p> {description} </p>
+        {theme}
+      </div>
   );
+
 }
 
 export default App;
